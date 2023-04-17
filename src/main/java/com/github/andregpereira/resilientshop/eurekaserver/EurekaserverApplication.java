72979b95e71d1f73467d5471cdb697c05c3c473e
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-04-17 14:53:06 -0300

    Projeto inicial

---
+++ src/main/java/com/github/andregpereira/resilientshop/eurekaserver/EurekaserverApplication.java
@@ -0,0 +1,13 @@
+package com.github.andregpereira.resilientshop.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/andregpereira/resilientshop/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.andregpereira.resilientshop.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
