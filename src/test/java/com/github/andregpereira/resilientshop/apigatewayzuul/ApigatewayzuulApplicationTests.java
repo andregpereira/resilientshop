a7dff703855ae740d13d81f2d3f03e23e794a824
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-04-18 11:24:19 -0300

    Projeto inicial

---
+++ src/test/java/com/github/andregpereira/resilientshop/apigatewayzuul/ApigatewayzuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.andregpereira.resilientshop.apigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayzuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
