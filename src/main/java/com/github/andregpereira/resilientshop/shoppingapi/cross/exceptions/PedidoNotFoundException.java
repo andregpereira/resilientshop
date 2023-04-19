651fb84e91e3fcfb4a569d138acca7f77bcbc954
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-04-19 18:50:36 -0300

    Refatorar exceção

--- src/main/java/com/github/andregpereira/resilientshop/shoppingapi/cross/exceptions/PedidoNotFoundException.java
+++ src/main/java/com/github/andregpereira/resilientshop/shoppingapi/cross/exceptions/PedidoNotFoundException.java
@@ -1,5 +1,6 @@
 package com.github.andregpereira.resilientshop.shoppingapi.cross.exceptions;
 
+import com.github.andregpereira.resilientshop.shoppingapi.infra.entities.StatusPedido;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
@@ -12,6 +13,11 @@ public class PedidoNotFoundException extends RuntimeException {
 
     public PedidoNotFoundException(Long id) {
         super("Opa! Não foi encontrado um pedido com o id " + id);
+    }
+
+    public PedidoNotFoundException(int status) {
+        super("Opa! Não foram encontrados pedidos com o status " + status + " (" + StatusPedido.getStatusPorId(
+                status) + ")");
     }
 
 }
