43b58ad2c9e8134c55429060866e9c3ae7abe25e
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-04-19 13:42:12 -0300

    Criar tratador de erros

---
+++ src/main/java/com/github/andregpereira/resilientshop/shoppingapi/cross/exceptions/PedidoNotFoundException.java
@@ -0,0 +1,9 @@
+package com.github.andregpereira.resilientshop.shoppingapi.cross.exceptions;
+
+public class PedidoNotFoundException extends RuntimeException {
+
+    public PedidoNotFoundException(String msg) {
+        super(msg);
+    }
+
+}
