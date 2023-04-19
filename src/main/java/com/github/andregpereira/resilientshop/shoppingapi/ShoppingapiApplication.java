b11f1fed4aa50e6b7a0c8863b8bbec191732015c
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-04-19 18:50:57 -0300

    Refatorar main

--- src/main/java/com/github/andregpereira/resilientshop/shoppingapi/ShoppingapiApplication.java
+++ src/main/java/com/github/andregpereira/resilientshop/shoppingapi/ShoppingapiApplication.java
@@ -2,12 +2,20 @@ package com.github.andregpereira.resilientshop.shoppingapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableEurekaServer
+@EnableFeignClients
+@EnableZuulProxy
 public class ShoppingapiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ShoppingapiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingapiApplication.class, args);
+    }
 
 }
