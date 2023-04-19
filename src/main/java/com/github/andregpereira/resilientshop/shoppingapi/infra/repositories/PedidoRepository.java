586e26885852a0d498c160de99bd4899d2d0ea4f
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-04-19 18:51:53 -0300

    Adicionar consulta por status

--- src/main/java/com/github/andregpereira/resilientshop/shoppingapi/infra/repositories/PedidoRepository.java
+++ src/main/java/com/github/andregpereira/resilientshop/shoppingapi/infra/repositories/PedidoRepository.java
@@ -1,8 +1,11 @@
 package com.github.andregpereira.resilientshop.shoppingapi.infra.repositories;
 
 import com.github.andregpereira.resilientshop.shoppingapi.infra.repositories.persistence.PedidoEntity;
-import com.github.andregpereira.resilientshop.shoppingapi.infra.entities.StatusPedido;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,6 +13,12 @@ import java.util.Optional;
 @Repository
 public interface PedidoRepository extends JpaRepository<PedidoEntity, Long> {
 
-    Optional<PedidoEntity> findByStatus(StatusPedido status);
+    Page<PedidoEntity> findAllByStatus(Integer status, Pageable pageable);
+
+    @Query(value = """
+            select * from tb_pedidos p
+            where p.id_pedido=:id and p.status=1
+            """, nativeQuery = true)
+    Optional<PedidoEntity> findByIdAndStatusAguardandoPagamento(@Param("id") Long id);
 
 }
