8e5d8dff225adc6f722e4dbd4244cfe949332a41
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-04-19 16:22:02 -0300

    Adicionar construtor com id e mensagem de não encontrado

--- src/main/java/com/github/andregpereira/resilientshop/shoppingapi/cross/exceptions/PedidoNotFoundException.java
+++ src/main/java/com/github/andregpereira/resilientshop/shoppingapi/cross/exceptions/PedidoNotFoundException.java
@@ -1,9 +1,17 @@
 package com.github.andregpereira.resilientshop.shoppingapi.cross.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class PedidoNotFoundException extends RuntimeException {
 
     public PedidoNotFoundException(String msg) {
         super(msg);
+    }
+
+    public PedidoNotFoundException(Long id) {
+        super("Opa! Não foi encontrado um pedido com o id " + id);
     }
 
 }
