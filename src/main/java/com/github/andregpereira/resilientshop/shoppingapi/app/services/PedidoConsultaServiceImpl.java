fcb477db1156a0f34a5d9ab222f6e4457ec4c966
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-04-19 16:18:31 -0300

    Refatorar serviços

--- src/main/java/com/github/andregpereira/resilientshop/shoppingapi/app/services/PedidoConsultaServiceImpl.java
+++ src/main/java/com/github/andregpereira/resilientshop/shoppingapi/app/services/PedidoConsultaServiceImpl.java
@@ -1,17 +1,23 @@
-package com.github.andregpereira.resilientshop.shoppingapi.app.service;
+package com.github.andregpereira.resilientshop.shoppingapi.app.services;
 
+import com.github.andregpereira.resilientshop.shoppingapi.app.dtos.pedido.PedidoDetalharDto;
 import com.github.andregpereira.resilientshop.shoppingapi.app.dtos.pedido.PedidoDto;
-import com.github.andregpereira.resilientshop.shoppingapi.app.dtos.produto.ProdutoDto;
-import com.github.andregpereira.resilientshop.shoppingapi.app.dtos.usuario.UsuarioDto;
-import com.github.andregpereira.resilientshop.shoppingapi.cross.consumers.ProdutoConsumer;
-import com.github.andregpereira.resilientshop.shoppingapi.cross.consumers.UsuarioConsumer;
+import com.github.andregpereira.resilientshop.shoppingapi.cross.exceptions.PedidoNotFoundException;
+import com.github.andregpereira.resilientshop.shoppingapi.cross.mappers.DetalhePedidoMapper;
 import com.github.andregpereira.resilientshop.shoppingapi.cross.mappers.PedidoMapper;
+import com.github.andregpereira.resilientshop.shoppingapi.cross.mappers.ProdutoMapper;
+import com.github.andregpereira.resilientshop.shoppingapi.infra.entities.Pedido;
+import com.github.andregpereira.resilientshop.shoppingapi.infra.feignclients.ProdutoFeignClient;
+import com.github.andregpereira.resilientshop.shoppingapi.infra.feignclients.UsuarioFeignClient;
 import com.github.andregpereira.resilientshop.shoppingapi.infra.repositories.PedidoRepository;
+import com.github.andregpereira.resilientshop.shoppingapi.infra.repositories.persistence.PedidoEntity;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -19,31 +25,36 @@ import org.springframework.stereotype.Service;
 public class PedidoConsultaServiceImpl implements PedidoConsultaService {
 
     private final PedidoRepository repository;
-    private final PedidoMapper mapper;
-    private final UsuarioConsumer usuarioConsumer;
-    private final ProdutoConsumer produtoConsumer;
+    private final PedidoMapper pedidoMapper;
+    private final DetalhePedidoMapper detalhePedidoMapper;
+    private final ProdutoMapper produtoMapper;
+    private final UsuarioFeignClient usuarioFeignClient;
+    private final ProdutoFeignClient produtoFeignClient;
 
     @Override
     public Page<PedidoDto> listar(Pageable pageable) {
-        // TODO
-        return null;
+        Page<PedidoEntity> pagePedidos = repository.findAll(pageable);
+        if (pagePedidos.isEmpty()) {
+            log.info("Nenhum pedido foi encontrado");
+            throw new PedidoNotFoundException("Poxa! Ainda não há pedidos cadastrados");
+        }
+        Page<Pedido> pedidos = pagePedidos.map(pedidoMapper::toPedido);
+        log.info("Retornando pedidos");
+        return pedidos.map(pedidoMapper::toPedidoDto);
     }
 
-    public PedidoDto consultarPorId(Long id) {
-        // TODO
-        return null;
-    }
-
-    @Override
-    public UsuarioDto consultarUsuarioPorId(Long id) {
-        // TODO
-        return usuarioConsumer.consultarPorId(id);
-    }
-
-    @Override
-    public ProdutoDto getUser(Long id) {
-        // TODO
-        return produtoConsumer.consultarPorId(id);
+    public PedidoDetalharDto consultarPorId(Long id) {
+        Optional<PedidoEntity> optionalPedido = repository.findById(id);
+        if (optionalPedido.isEmpty()) {
+            log.info("Pedido com id {} não encontrado", id);
+            throw new PedidoNotFoundException(id);
+        }
+        Pedido pedido = pedidoMapper.toPedido(optionalPedido.get());
+        log.info("Setando produto(s)...");
+        pedido.getDetalhePedido().parallelStream().forEach(
+                dp -> dp.setProduto(produtoMapper.toProduto(produtoFeignClient.consultarPorId(dp.getIdProduto()))));
+        log.info("Retornando pedido");
+        return pedidoMapper.toPedidoDetalharDto(pedido);
     }
 
 }
