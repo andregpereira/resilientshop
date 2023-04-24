58705ffb3500b0604f57cfb1702abf019f7f3dbd
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-04-24 15:44:54 -0300

    Renomear projeto, pacote e main

--- src/main/java/com/github/andregpereira/resilientshop/apigateway/ApigatewayApplication.java
+++ src/main/java/com/github/andregpereira/resilientshop/apigateway/ApigatewayApplication.java
@@ -1,4 +1,4 @@
-package com.github.andregpereira.resilientshop.apigatewayzuul;
+package com.github.andregpereira.resilientshop.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class ApigatewayzuulApplication {
+public class ApigatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApigatewayzuulApplication.class, args);
+        SpringApplication.run(ApigatewayApplication.class, args);
     }
 
 }
