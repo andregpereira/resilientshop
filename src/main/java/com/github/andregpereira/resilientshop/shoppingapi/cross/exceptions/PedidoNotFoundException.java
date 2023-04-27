fc95862d410d1d080bf70285d9d60fce39a5905a
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-04-27 00:27:59 -0300

    Refatorar exception e formatar mensagens com MessageFormat.format()

--- src/main/java/com/github/andregpereira/resilientshop/shoppingapi/cross/exceptions/PedidoNotFoundException.java
+++ src/main/java/com/github/andregpereira/resilientshop/shoppingapi/cross/exceptions/PedidoNotFoundException.java
@@ -4,6 +4,8 @@ import com.github.andregpereira.resilientshop.shoppingapi.infra.entities.StatusP
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.text.MessageFormat;
+
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class PedidoNotFoundException extends RuntimeException {
 
@@ -12,12 +14,12 @@ public class PedidoNotFoundException extends RuntimeException {
     }
 
     public PedidoNotFoundException(Long id) {
-        super("Opa! Não foi encontrado um pedido com o id " + id);
+        super(MessageFormat.format("Opa! Não foi encontrado um pedido com o id {0}", id));
     }
 
     public PedidoNotFoundException(int status) {
-        super("Opa! Não foram encontrados pedidos com o status " + status + " (" + StatusPedido.getStatusPorId(
-                status) + ")");
+        super(MessageFormat.format("Opa! Não foram encontrados pedidos com o status {0} ({1})", status,
+                StatusPedido.getStatusPorId(status)));
     }
 
 }
