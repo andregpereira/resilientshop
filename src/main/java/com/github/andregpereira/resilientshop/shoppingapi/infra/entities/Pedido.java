ae9d20f23e7b153429fab607cdb529e9c30cce3f
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-04-28 20:10:08 -0300

    Adicionar métodos equals() hashCode() e toString()

--- src/main/java/com/github/andregpereira/resilientshop/shoppingapi/infra/entities/Pedido.java
+++ src/main/java/com/github/andregpereira/resilientshop/shoppingapi/infra/entities/Pedido.java
@@ -8,6 +8,8 @@ import lombok.Setter;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
+import java.util.StringJoiner;
 
 @Getter
 @Setter
@@ -21,5 +23,29 @@ public class Pedido {
     private int status;
     private BigDecimal total;
     private List<DetalhePedido> detalhePedido;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Pedido pedido = (Pedido) o;
+        return status == pedido.status && Objects.equals(id, pedido.id) && Objects.equals(dataCriacao,
+                pedido.dataCriacao) && Objects.equals(dataModificacao, pedido.dataModificacao) && Objects.equals(total,
+                pedido.total) && Objects.equals(detalhePedido, pedido.detalhePedido);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, dataCriacao, dataModificacao, status, total, detalhePedido);
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Pedido.class.getSimpleName() + "[", "]").add("id=" + id).add(
+                "dataCriacao=" + dataCriacao).add("dataModificacao=" + dataModificacao).add("status=" + status).add(
+                "total=" + total).add("detalhePedido=" + detalhePedido).toString();
+    }
 
 }
