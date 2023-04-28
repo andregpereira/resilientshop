f1dc263337e8f61040f45f8440645afd8fd90e2f
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-04-28 20:12:52 -0300

    Adicionar @CircuitBreaker e métodos fallback

--- src/main/java/com/github/andregpereira/resilientshop/shoppingapi/app/controllers/PedidoController.java
+++ src/main/java/com/github/andregpereira/resilientshop/shoppingapi/app/controllers/PedidoController.java
@@ -6,6 +6,8 @@ import com.github.andregpereira.resilientshop.shoppingapi.app.dtos.pedido.Pedido
 import com.github.andregpereira.resilientshop.shoppingapi.app.services.PedidoConsultaService;
 import com.github.andregpereira.resilientshop.shoppingapi.app.services.PedidoManutencaoService;
 import com.github.andregpereira.resilientshop.shoppingapi.infra.entities.StatusPedido;
+import feign.FeignException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,11 +15,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.text.MessageFormat;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -29,6 +33,7 @@ public class PedidoController {
     private final PedidoConsultaService consultaService;
 
     @PostMapping
+    @CircuitBreaker(name = "cadastrar", fallbackMethod = "cadastrarFallbackMethod")
     public ResponseEntity<PedidoDetalharDto> criar(@RequestBody @Valid PedidoRegistrarDto dto,
             UriComponentsBuilder uriBuilder) {
         log.info("Criando pedido...");
@@ -36,6 +41,12 @@ public class PedidoController {
         URI uri = uriBuilder.path("/pedidos/{id}").buildAndExpand(pedido.id()).toUri();
         log.info("Pedido criado com sucesso");
         return ResponseEntity.created(uri).body(pedido);
+    }
+
+    public ResponseEntity<String> cadastrarFallbackMethod(Long id, FeignException.ServiceUnavailable e) {
+        log.warn(MessageFormat.format("Erro ao tentar acessar a API de produto: id :{0} {1}", id, e));
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(
+                MessageFormat.format("Opa! Não foi possível cadastrar o pedido: {0}", id));
     }
 
     @DeleteMapping("/{id}")
@@ -52,9 +63,16 @@ public class PedidoController {
     }
 
     @GetMapping("/{id}")
+    @CircuitBreaker(name = "consultarPorId", fallbackMethod = "consultarPorIdFallbackMethod")
     public ResponseEntity<PedidoDetalharDto> consultarPorId(@PathVariable Long id) {
         log.info("Pesquisando pedido com id {}...", id);
         return ResponseEntity.ok(consultaService.consultarPorId(id));
+    }
+
+    public ResponseEntity<String> consultarPorIdFallbackMethod(Long id, Exception e) {
+        log.warn(MessageFormat.format("Erro ao tentar acessar a API de produto: id :{0} {1}", id, e));
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(
+                MessageFormat.format("Opa! Não foi possível consultar o pedido: {0}", id));
     }
 
     @GetMapping("/status")
