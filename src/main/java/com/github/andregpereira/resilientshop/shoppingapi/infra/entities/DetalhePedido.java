33671e7ada066a7f1c2c20db40e2dba2827181fa
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-04-28 20:10:08 -0300

    Adicionar métodos equals() hashCode() e toString()

--- src/main/java/com/github/andregpereira/resilientshop/shoppingapi/infra/entities/DetalhePedido.java
+++ src/main/java/com/github/andregpereira/resilientshop/shoppingapi/infra/entities/DetalhePedido.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
+import java.util.Objects;
+import java.util.StringJoiner;
 
 @Getter
 @Setter
@@ -18,5 +20,28 @@ public class DetalhePedido {
     private BigDecimal subtotal;
     private Long idProduto;
     private Produto produto;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        DetalhePedido that = (DetalhePedido) o;
+        return quantidade == that.quantidade && Objects.equals(id, that.id) && Objects.equals(subtotal,
+                that.subtotal) && Objects.equals(idProduto, that.idProduto) && Objects.equals(produto, that.produto);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, quantidade, subtotal, idProduto, produto);
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", DetalhePedido.class.getSimpleName() + "[", "]").add("id=" + id).add(
+                "quantidade=" + quantidade).add("subtotal=" + subtotal).add("idProduto=" + idProduto).add(
+                "produto=" + produto).toString();
+    }
 
 }
