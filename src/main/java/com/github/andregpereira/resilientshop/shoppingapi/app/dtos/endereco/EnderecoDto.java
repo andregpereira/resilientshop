1b674db3281fb4507a863bdd88afed20bd6295c0
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-05-02 10:56:27 -0300

    Refatorar DTOs

--- src/main/java/com/github/andregpereira/resilientshop/shoppingapi/app/dtos/endereco/EnderecoDto.java
+++ src/main/java/com/github/andregpereira/resilientshop/shoppingapi/app/dtos/endereco/EnderecoDto.java
@@ -1,6 +1,7 @@
 package com.github.andregpereira.resilientshop.shoppingapi.app.dtos.endereco;
 
 public record EnderecoDto(Long id,
+        String apelido,
         String cep,
         String estado,
         String cidade,
--- src/main/java/com/github/andregpereira/resilientshop/shoppingapi/app/dtos/pedido/PedidoDetalharDto.java
+++ src/main/java/com/github/andregpereira/resilientshop/shoppingapi/app/dtos/pedido/PedidoDetalharDto.java
@@ -2,6 +2,7 @@ package com.github.andregpereira.resilientshop.shoppingapi.app.dtos.pedido;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.github.andregpereira.resilientshop.shoppingapi.app.dtos.detalhepedido.DetalhePedidoDto;
+import com.github.andregpereira.resilientshop.shoppingapi.app.dtos.usuario.UsuarioDto;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -12,6 +13,7 @@ public record PedidoDetalharDto(Long id,
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/uuuu HH:mm") LocalDateTime dataModificacao,
         int status,
         BigDecimal total,
+        UsuarioDto usuario,
         List<DetalhePedidoDto> detalhePedido) {
 
 }
