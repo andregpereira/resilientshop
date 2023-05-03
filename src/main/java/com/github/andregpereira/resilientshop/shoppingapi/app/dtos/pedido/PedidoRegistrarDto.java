b57d335685ad8ce4666297f5855a56b86bea7092
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-05-03 15:00:23 -0300

    Adicionar apelido do endereço

--- src/main/java/com/github/andregpereira/resilientshop/shoppingapi/app/dtos/pedido/PedidoRegistrarDto.java
+++ src/main/java/com/github/andregpereira/resilientshop/shoppingapi/app/dtos/pedido/PedidoRegistrarDto.java
@@ -1,6 +1,7 @@
 package com.github.andregpereira.resilientshop.shoppingapi.app.dtos.pedido;
 
 import com.github.andregpereira.resilientshop.shoppingapi.app.dtos.detalhepedido.DetalhePedidoRegistrarDto;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
@@ -8,6 +9,7 @@ import java.util.List;
 
 public record PedidoRegistrarDto(@NotNull(message = "O usuário é obrigatório") @Positive(
         message = "O valor mínimo para esse campo é 1") Long idUsuario,
+        @NotBlank(message = "O apelido do endereço é obrigatório") String apelidoEndereco,
         @NotNull(message = "Os detalhes do pedido são obrigatórios") List<DetalhePedidoRegistrarDto> detalhePedido) {
 
 }
