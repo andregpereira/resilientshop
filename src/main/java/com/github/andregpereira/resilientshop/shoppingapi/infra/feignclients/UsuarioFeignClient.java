ae718f92a380d2f0ebced4a66eafbc462b29f9a6
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-05-03 15:03:27 -0300

    Adicionar consulta por apelido de endereço

--- src/main/java/com/github/andregpereira/resilientshop/shoppingapi/infra/feignclients/UsuarioFeignClient.java
+++ src/main/java/com/github/andregpereira/resilientshop/shoppingapi/infra/feignclients/UsuarioFeignClient.java
@@ -1,16 +1,21 @@
 package com.github.andregpereira.resilientshop.shoppingapi.infra.feignclients;
 
+import com.github.andregpereira.resilientshop.shoppingapi.app.dtos.endereco.EnderecoDto;
 import com.github.andregpereira.resilientshop.shoppingapi.app.dtos.usuario.UsuarioDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Component
-@FeignClient(name = "resilientshop-user-api", path = "/usuarios")
+@FeignClient(name = "resilientshop-user-api")
 public interface UsuarioFeignClient {
 
-    @GetMapping("/{id}")
+    @GetMapping("/usuarios/{id}")
     UsuarioDto consultarPorId(@PathVariable Long id);
+
+    @GetMapping("/enderecos/usuario/{idUsuario}/apelido")
+    EnderecoDto consultarPorApelido(@PathVariable Long idUsuario, @RequestParam String apelido);
 
 }
