f0cb49ec5b478cb3dfdb7634c2aa73267fb4841b
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-05-17 02:59:15 -0300

    Refatorar código

--- src/main/java/com/github/andregpereira/resilientshop/shoppingapi/infra/feignclients/ProdutosFeignClient.java
+++ src/main/java/com/github/andregpereira/resilientshop/shoppingapi/infra/feignclients/ProdutosFeignClient.java
@@ -3,22 +3,21 @@ package com.github.andregpereira.resilientshop.shoppingapi.infra.feignclients;
 import com.github.andregpereira.resilientshop.shoppingapi.app.dtos.produto.ProdutoAtualizarEstoqueDto;
 import com.github.andregpereira.resilientshop.shoppingapi.app.dtos.produto.ProdutoDto;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
+import java.util.List;
+
 @Component
 @FeignClient(name = "resilientshop-products-api")
 public interface ProdutosFeignClient {
 
-    @GetMapping(value = "/produtos/{id}", produces = MediaType.APPLICATION_JSON_VALUE,
-            consumes = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping("/produtos/{id}")
     ProdutoDto consultarPorId(@PathVariable Long id);
 
-    @PutMapping(value = "/produtos/subtrair/{id}", produces = MediaType.APPLICATION_JSON_VALUE,
-            consumes = MediaType.APPLICATION_JSON_VALUE)
-    void subtrair(@PathVariable Long id, ProdutoAtualizarEstoqueDto dto);
+    @PutMapping("/produtos/subtrair")
+    void subtrair(List<ProdutoAtualizarEstoqueDto> dto);
 
 }
