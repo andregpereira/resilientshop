556bdb6f45f851be73232b1d097513089bd55ade
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-05-17 03:04:25 -0300

    Refatorar atributos

--- src/main/java/com/github/andregpereira/resilientshop/shoppingapi/app/dtos/detalhepedido/DetalhePedidoRegistrarDto.java
+++ src/main/java/com/github/andregpereira/resilientshop/shoppingapi/app/dtos/detalhepedido/DetalhePedidoRegistrarDto.java
@@ -3,9 +3,9 @@ package com.github.andregpereira.resilientshop.shoppingapi.app.dtos.detalhepedid
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
-public record DetalhePedidoRegistrarDto(@NotNull(message = "A quantidade do produto é obrigatória") @Positive(
-        message = "Ops! Quantidade inválida, digite um número que não seja negativo ou zero") int quantidade,
-        @NotNull(message = "O produto é obrigatório") @Positive(
-                message = "O valor mínimo para esse campo é 1") Long idProduto) {
+public record DetalhePedidoRegistrarDto(@NotNull(message = "O produto é obrigatório") @Positive(
+        message = "O valor mínimo para esse campo é 1") Long idProduto,
+        @NotNull(message = "A quantidade do produto é obrigatória") @Positive(
+                message = "Ops! Quantidade inválida, digite um número que não seja negativo ou zero") int quantidade) {
 
 }
