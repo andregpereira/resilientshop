6fe4a78087dc9ddfc576b98b0437ca4c4d4d5f90
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-05-30 00:18:13 -0300

    Remover atributo EnderecoDto

--- src/main/java/com/github/andregpereira/resilientshop/shoppingapi/app/dto/usuario/UsuarioDto.java
+++ src/main/java/com/github/andregpereira/resilientshop/shoppingapi/app/dto/usuario/UsuarioDto.java
@@ -1,9 +1,6 @@
 package com.github.andregpereira.resilientshop.shoppingapi.app.dto.usuario;
 
-import com.github.andregpereira.resilientshop.shoppingapi.app.dto.endereco.EnderecoDto;
-
 public record UsuarioDto(Long id,
-        String nome,
-        EnderecoDto endereco) {
+        String nome) {
 
 }
