213be9123fa1d9cdc2d3cca9691d583e2ddf3aaf
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-05-30 10:57:22 -0300

    Criar Javadoc

--- src/main/java/com/github/andregpereira/resilientshop/shoppingapi/app/services/PedidoConsultaServiceImpl.java
+++ src/main/java/com/github/andregpereira/resilientshop/shoppingapi/app/services/PedidoConsultaServiceImpl.java
@@ -7,6 +7,8 @@ import com.github.andregpereira.resilientshop.shoppingapi.cross.mappers.PedidoMa
 import com.github.andregpereira.resilientshop.shoppingapi.infra.feignclients.ProdutosFeignClient;
 import com.github.andregpereira.resilientshop.shoppingapi.infra.feignclients.UsuariosFeignClient;
 import com.github.andregpereira.resilientshop.shoppingapi.infra.repositories.PedidoRepository;
+import com.github.andregpereira.resilientshop.shoppingapi.infra.repositories.persistence.PedidoEntity;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -17,16 +19,69 @@ import java.util.Optional;
 
 import static java.util.function.Predicate.not;
 
+/**
+ * Classe de serviço de consulta de {@link PedidoEntity}.
+ *
+ * @author André Garcia
+ * @see PedidoConsultaService
+ */
 @RequiredArgsConstructor
 @Slf4j
 @Service
 public class PedidoConsultaServiceImpl implements PedidoConsultaService {
 
+    /**
+     * Injeção da dependência {@link PedidoRepository} para realizar operações de
+     * consulta na tabela de pedidos no banco de dados.
+     */
     private final PedidoRepository repository;
+
+    /**
+     * Injeção da dependência {@link PedidoMapper} para realizar
+     * conversões de entidade para DTO de pedidos.
+     */
     private final PedidoMapper pedidoMapper;
+
+    /**
+     * Injeção da dependência {@link UsuariosFeignClient} para realizar
+     * requisoções na API de Usuários.
+     */
     private final UsuariosFeignClient usuariosFeignClient;
+
+    /**
+     * Injeção da dependência {@link ProdutosFeignClient} para realizar
+     * requisoções na API de Produtos.
+     */
     private final ProdutosFeignClient produtosFeignClient;
 
+    /**
+     * Lista todos os {@linkplain PedidoEntity pedidos} cadastrados.
+     * Retorna uma {@linkplain Page sublista} de uma lista de {@link PedidoDto pedidos}.
+     *
+     * @param pageable o {@code pageable} padrão.
+     *
+     * @return uma sublista de uma lista com todos os pedidos cadastrados.
+     */
+    @Override
+    public Page<PedidoDto> listar(Pageable pageable) {
+        return Optional.of(repository.findAll(pageable)).filter(not(Page::isEmpty)).map(p -> {
+            log.info("Retornando todos os pedidos");
+            return p.map(pedidoMapper::toPedidoDto);
+        }).orElseThrow(() -> {
+            log.info("Nenhum pedido foi encontrado");
+            return new PedidoNotFoundException();
+        });
+    }
+
+    /**
+     * Lista todos os {@linkplain PedidoEntity pedidos} de um usuário.
+     * Retorna uma {@linkplain Page sublista} de uma lista de {@link PedidoDto pedidos}.
+     *
+     * @param id       o {@code id} do usuário.
+     * @param pageable o {@code pageable} padrão.
+     *
+     * @return uma sublista de uma lista com todos os pedidos do usuário.
+     */
     @Override
     public Page<PedidoDto> listarPorUsuario(Long id, Pageable pageable) {
         log.info("Procurando usuário com id {}...", id);
@@ -41,17 +96,18 @@ public class PedidoConsultaServiceImpl implements PedidoConsultaService {
         });
     }
 
-    @Override
-    public Page<PedidoDto> listar(Pageable pageable) {
-        return Optional.of(repository.findAll(pageable)).filter(not(Page::isEmpty)).map(p -> {
-            log.info("Retornando todos os pedidos");
-            return p.map(pedidoMapper::toPedidoDto);
-        }).orElseThrow(() -> {
-            log.info("Nenhum pedido foi encontrado");
-            return new PedidoNotFoundException();
-        });
-    }
-
+    /**
+     * Pesquisa um {@linkplain PedidoEntity pedido} por {@code id}.
+     * Retorna um {@linkplain PedidoDetalharDto pedido detalhado}.
+     *
+     * @param id o {@code id} do pedido a ser consultado.
+     *
+     * @return um pedido encontrado pelo {@code id}.
+     *
+     * @throws PedidoNotFoundException           caso o pedido não seja encontrado.
+     * @throws FeignException.NotFound           caso o usuário ou os produtos não sejam encontrados.
+     * @throws FeignException.ServiceUnavailable caso a API de Uusários ou Produtos estejam indisponíveis.
+     */
     @Override
     public PedidoDetalharDto consultarPorId(Long id) {
         return repository.findById(id).map(p -> {
