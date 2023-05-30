4c1f26f461ed89c3751240e2c6b4a58282e783e3
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-05-29 23:47:40 -0300

    Add entidade da biblioteca Commons

--- src/main/java/com/github/andregpereira/resilientshop/shoppingapi/infra/repositories/persistence/DetalhePedidoEntity.java
+++ src/main/java/com/github/andregpereira/resilientshop/shoppingapi/infra/repositories/persistence/DetalhePedidoEntity.java
@@ -1,6 +1,6 @@
 package com.github.andregpereira.resilientshop.shoppingapi.infra.repositories.persistence;
 
-import com.github.andregpereira.resilientshop.shoppingapi.infra.entities.Produto;
+import com.github.andregpereira.resilientshop.commons.entities.Produto;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
