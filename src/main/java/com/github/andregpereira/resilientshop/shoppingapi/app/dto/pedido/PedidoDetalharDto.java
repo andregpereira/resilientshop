2be5f61597ece73f640e300caf6cdfe1fb6485bb
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-05-30 00:19:21 -0300

    Refatorar código e add atributo EnderecoDto

--- src/main/java/com/github/andregpereira/resilientshop/shoppingapi/app/dto/pedido/PedidoDetalharDto.java
+++ src/main/java/com/github/andregpereira/resilientshop/shoppingapi/app/dto/pedido/PedidoDetalharDto.java
@@ -1,7 +1,9 @@
 package com.github.andregpereira.resilientshop.shoppingapi.app.dto.pedido;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
 import com.github.andregpereira.resilientshop.shoppingapi.app.dto.detalhepedido.DetalhePedidoDto;
+import com.github.andregpereira.resilientshop.shoppingapi.app.dto.endereco.EnderecoDto;
 import com.github.andregpereira.resilientshop.shoppingapi.app.dto.usuario.UsuarioDto;
 
 import java.math.BigDecimal;
@@ -9,11 +11,12 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public record PedidoDetalharDto(Long id,
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/uuuu HH:mm") LocalDateTime dataCriacao,
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/uuuu HH:mm") LocalDateTime dataModificacao,
+        @JsonFormat(shape = Shape.STRING, pattern = "dd/MM/uuuu HH:mm") LocalDateTime dataCriacao,
+        @JsonFormat(shape = Shape.STRING, pattern = "dd/MM/uuuu HH:mm") LocalDateTime dataModificacao,
         int status,
         BigDecimal total,
         UsuarioDto usuario,
+        EnderecoDto endereco,
         List<DetalhePedidoDto> detalhePedido) {
 
 }
