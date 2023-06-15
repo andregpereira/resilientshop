67bbe72e47edf251bf6f2580ff78227c149f6c4c
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-06-15 09:34:55 -0300

    Add configurações

---
+++ src/main/java/com/github/andregpereira/resilientshop/authenticationapi/app/dto/AuthRequestDto.java
@@ -0,0 +1,6 @@
+package com.github.andregpereira.resilientshop.authenticationapi.app.dto;
+
+public record AuthRequestDto(String email,
+        String senha) {
+
+}
---
+++ src/main/java/com/github/andregpereira/resilientshop/authenticationapi/infra/repository/UserCredentialRepository.java
@@ -0,0 +1,14 @@
+package com.github.andregpereira.resilientshop.authenticationapi.infra.repository;
+
+import com.github.andregpereira.resilientshop.authenticationapi.infra.entity.UsuarioCredential;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserCredentialRepository extends JpaRepository<UsuarioCredential, Long> {
+
+    Optional<UsuarioCredential> findByEmail(String email);
+
+}
