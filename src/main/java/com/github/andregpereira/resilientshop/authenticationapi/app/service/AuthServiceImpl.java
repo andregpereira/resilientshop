5c9c4aec738338fcd6ba5fa425408b613a5e2de6
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-06-19 10:08:24 -0300

    Refatorar service

--- src/main/java/com/github/andregpereira/resilientshop/authenticationapi/app/service/AuthServiceImpl.java
+++ src/main/java/com/github/andregpereira/resilientshop/authenticationapi/app/service/AuthServiceImpl.java
@@ -1,34 +1,40 @@
 package com.github.andregpereira.resilientshop.authenticationapi.app.service;
 
-import com.github.andregpereira.resilientshop.authenticationapi.infra.entity.UsuarioCredential;
+import com.github.andregpereira.resilientshop.authenticationapi.app.dto.UsuarioCredentialDto;
+import com.github.andregpereira.resilientshop.authenticationapi.app.dto.UsuarioCredentialRegistroDto;
+import com.github.andregpereira.resilientshop.authenticationapi.app.mapper.UsuarioCredentialMapper;
+import com.github.andregpereira.resilientshop.authenticationapi.domain.entity.UsuarioCredential;
 import com.github.andregpereira.resilientshop.authenticationapi.infra.repository.UserCredentialRepository;
-import com.github.andregpereira.resilientshop.authenticationapi.security.service.JwtService;
+import com.github.andregpereira.resilientshop.authenticationapi.infra.security.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
+@Slf4j
 @Service
-public class AuthServiceImpl {
+public class AuthServiceImpl implements AuthService {
 
     private final UserCredentialRepository repository;
+    private final UsuarioCredentialMapper mapper;
     private final PasswordEncoder passwordEncoder;
-    private final JwtService jwtService;
+    private final JwtProvider jwtProvider;
 
-    public String criar(UsuarioCredential credential) {
-        credential.setSenha(passwordEncoder.encode(credential.getSenha()));
-        credential.setRole("admin");
-        repository.save(credential);
-        return "user added to the system";
+    public UsuarioCredentialDto criar(UsuarioCredentialRegistroDto dto) {
+        UsuarioCredential usuario = mapper.toUsuarioCredential(dto);
+        usuario.setSenha(passwordEncoder.encode(dto.senha()));
+        usuario.setRole("admin");
+        return mapper.toUsuarioCredentialDto(repository.save(usuario));
     }
 
     public String gerarToken(String username) {
-        return repository.findByEmail(username).map(jwtService::generateToken).orElseThrow(
+        return repository.findByEmail(username).map(jwtProvider::generateToken).orElseThrow(
                 () -> new RuntimeException("erro ao gerar token"));
     }
 
     public void validarToken(String token) {
-        jwtService.validateToken(token);
+        jwtProvider.validateToken(token);
     }
 
 }
