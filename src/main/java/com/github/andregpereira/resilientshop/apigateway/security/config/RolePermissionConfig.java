690de12f9f83474afbabbb6bdff08c73b338b0e2
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-06-21 15:47:22 -0300

    Atualizar rotas de roles

--- src/main/java/com/github/andregpereira/resilientshop/apigateway/security/config/RolePermissionConfig.java
+++ src/main/java/com/github/andregpereira/resilientshop/apigateway/security/config/RolePermissionConfig.java
@@ -16,19 +16,29 @@ public class RolePermissionConfig {
 
     public RolePermissionConfig() {
         Map<HttpMethod, List<String>> adminPermissions = new HashMap<>();
-        adminPermissions.put(HttpMethod.GET, Arrays.asList("/pedidos/**", "/produtos/**", "/usuarios/**", "/auth/**"));
-        adminPermissions.put(HttpMethod.POST, Arrays.asList("/pedidos/**", "/produtos/**", "/usuarios/**", "/auth/**"));
-        adminPermissions.put(HttpMethod.PUT, Arrays.asList("/pedidos/**", "/produtos/**", "/usuarios/**", "/auth/**"));
+        adminPermissions.put(HttpMethod.GET,
+                Arrays.asList("/pedidos/**", "/produtos/**", "/subcategorias/**", "/categorias/**", "/usuarios/**",
+                        "/enderecos/**", "/auth/**"));
+        adminPermissions.put(HttpMethod.POST,
+                Arrays.asList("/pedidos/**", "/produtos/**", "/subcategorias/**", "/categorias/**", "/usuarios/**",
+                        "/enderecos/**", "/auth/**"));
+        adminPermissions.put(HttpMethod.PUT,
+                Arrays.asList("/pedidos/**", "/produtos/**", "/subcategorias/**", "/categorias/**", "/usuarios/**",
+                        "/enderecos/**", "/auth/**"));
         adminPermissions.put(HttpMethod.DELETE,
                 Arrays.asList("/pedidos/**", "/produtos/**", "/usuarios/**", "/auth/**"));
-        adminPermissions.put(HttpMethod.PATCH, Arrays.asList("/usuarios/**"));
+        adminPermissions.put(HttpMethod.PATCH, List.of("/usuarios/**"));
         rolePermissions.put("admin", new RolePermissions(adminPermissions));
         Map<HttpMethod, List<String>> userPermissions = new HashMap<>();
-        userPermissions.put(HttpMethod.GET, Arrays.asList("/produtos/**", "/usuarios/**"));
-        userPermissions.put(HttpMethod.POST, Arrays.asList("/usuarios/**"));
+        userPermissions.put(HttpMethod.GET,
+                Arrays.asList("/produtos/**", "/usuarios/{id}", "/enderecos/usuario/{idUsuario}",
+                        "/enderecos/usuario/{idUsuario}/apelido", "/pedidos/**"));
+        userPermissions.put(HttpMethod.POST, Arrays.asList("/usuarios/**", "/enderecos/**", "/pedidos/**"));
+        userPermissions.put(HttpMethod.PUT, Arrays.asList("/usuarios/**", "/enderecos/**"));
+        userPermissions.put(HttpMethod.PATCH, List.of("/usuarios/**"));
         rolePermissions.put("user", new RolePermissions(userPermissions));
         Map<HttpMethod, List<String>> anonymousPermissions = new HashMap<>();
-        anonymousPermissions.put(HttpMethod.GET, Arrays.asList("/produtos/**", "/auth/**"));
+        anonymousPermissions.put(HttpMethod.GET, List.of("/produtos/**"));
         anonymousPermissions.put(HttpMethod.POST, Arrays.asList("/usuarios/**", "/auth/**"));
         rolePermissions.put("anonymous", new RolePermissions(anonymousPermissions));
     }
