a5babc64f1b5aee0b48b3a2f730c5d943a243488
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-06-21 14:45:10 -0300

    Criar e implementar interface

--- src/main/java/com/github/andregpereira/resilientshop/authenticationapi/app/service/AuthService.java
+++ src/main/java/com/github/andregpereira/resilientshop/authenticationapi/app/service/AuthService.java
@@ -1,5 +1,6 @@
 package com.github.andregpereira.resilientshop.authenticationapi.app.service;
 
+import com.github.andregpereira.resilientshop.authenticationapi.app.dto.LoginDto;
 import com.github.andregpereira.resilientshop.authenticationapi.app.dto.UsuarioCredentialDto;
 import com.github.andregpereira.resilientshop.authenticationapi.app.dto.UsuarioCredentialRegistroDto;
 
@@ -7,7 +8,7 @@ public interface AuthService {
 
     UsuarioCredentialDto criar(UsuarioCredentialRegistroDto dto);
 
-    String gerarToken(String username);
+    String gerarToken(LoginDto username);
 
     void validarToken(String token);
 
--- src/main/java/com/github/andregpereira/resilientshop/authenticationapi/app/service/AuthServiceImpl.java
+++ src/main/java/com/github/andregpereira/resilientshop/authenticationapi/app/service/AuthServiceImpl.java
@@ -1,5 +1,6 @@
 package com.github.andregpereira.resilientshop.authenticationapi.app.service;
 
+import com.github.andregpereira.resilientshop.authenticationapi.app.dto.LoginDto;
 import com.github.andregpereira.resilientshop.authenticationapi.app.dto.UsuarioCredentialDto;
 import com.github.andregpereira.resilientshop.authenticationapi.app.dto.UsuarioCredentialRegistroDto;
 import com.github.andregpereira.resilientshop.authenticationapi.app.mapper.UsuarioCredentialMapper;
@@ -8,6 +9,9 @@ import com.github.andregpereira.resilientshop.authenticationapi.infra.repository
 import com.github.andregpereira.resilientshop.authenticationapi.infra.security.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -18,9 +22,11 @@ public class AuthServiceImpl implements AuthService {
 
     private final UserCredentialRepository repository;
     private final UsuarioCredentialMapper mapper;
+    private final AuthenticationManager authenticationManager;
     private final PasswordEncoder passwordEncoder;
     private final JwtProvider jwtProvider;
 
+    @Override
     public UsuarioCredentialDto criar(UsuarioCredentialRegistroDto dto) {
         UsuarioCredential usuario = mapper.toUsuarioCredential(dto);
         usuario.setSenha(passwordEncoder.encode(dto.senha()));
@@ -28,13 +34,29 @@ public class AuthServiceImpl implements AuthService {
         return mapper.toUsuarioCredentialDto(repository.save(usuario));
     }
 
-    public String gerarToken(String username) {
-        return repository.findByEmail(username).map(jwtProvider::generateToken).orElseThrow(
-                () -> new RuntimeException("erro ao gerar token"));
+    @Override
+    public String gerarToken(LoginDto dto) {
+        try {
+            log.info("auth1");
+            Authentication authenticate = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(dto.email(), dto.senha()));
+            log.info("auth2");
+            if (authenticate.isAuthenticated()) {
+                log.info(repository.toString());
+                return repository.findByEmail(dto.email()).map(jwtProvider::gerarToken).orElseThrow(
+                        () -> new RuntimeException("erro ao gerar token"));
+            } else {
+                throw new RuntimeException("invalid access");
+            }
+        } catch (Exception e) {
+            log.info(e.getMessage());
+            return null;
+        }
     }
 
+    @Override
     public void validarToken(String token) {
-        jwtProvider.validateToken(token);
+        jwtProvider.validarToken(token);
     }
 
 }
