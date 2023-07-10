606d91481c4d55bbe3ab4d73711043512323206f
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-07-10 18:19:30 -0300

    Criar entidade de negócio Cupom

---
+++ resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/domain/model/Cupom.java
@@ -0,0 +1,17 @@
+package com.github.andregpereira.resilientshop.discountsapi.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cupom {
+
+    Long id;
+    String codigo;
+
+}
