894b7499e73b4bd2520defcbba4da79768e282a6
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-07-10 17:33:16 -0300

    Criar API de descontos

---
+++ resilientshop-discounts-api/src/test/java/com/github/andregpereira/resilientshop/discountsapi/DiscountsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.andregpereira.resilientshop.discountsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscountsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
