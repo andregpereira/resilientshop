b609d9ff98b9ff7f4b547d8288004b7e038ce852
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-07-10 17:33:16 -0300

    Criar API de descontos

---
+++ resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/DiscountsApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.andregpereira.resilientshop.discountsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscountsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscountsApiApplication.class, args);
+	}
+
+}
