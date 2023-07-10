85d79df928a718532376e624726ddbf0fb02acef
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-07-10 18:20:02 -0300

    Criar use cases de cupom

---
+++ resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/domain/usecase/CreateCupom.java
@@ -0,0 +1,9 @@
+package com.github.andregpereira.resilientshop.discountsapi.domain.usecase;
+
+import com.github.andregpereira.resilientshop.discountsapi.domain.model.Cupom;
+
+public interface CreateCupom {
+
+    Cupom criar(Cupom cupom);
+
+}
---
+++ resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/domain/usecase/CreateCupomImpl.java
@@ -0,0 +1,19 @@
+package com.github.andregpereira.resilientshop.discountsapi.domain.usecase;
+
+import com.github.andregpereira.resilientshop.discountsapi.domain.gateway.CupomGateway;
+import com.github.andregpereira.resilientshop.discountsapi.domain.model.Cupom;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class CreateCupomImpl implements CreateCupom {
+
+    private final CupomGateway gateway;
+
+    @Override
+    public Cupom criar(Cupom cupom) {
+        return gateway.registrar(cupom);
+    }
+
+}
