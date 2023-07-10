34f417adf086f102ea99fbe275e47bdada8a913a
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-07-10 18:22:56 -0300

    Criar REST controller de cupom

---
+++ resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/app/rest/CupomController.java
@@ -0,0 +1,34 @@
+package com.github.andregpereira.resilientshop.discountsapi.app.rest;
+
+import com.github.andregpereira.resilientshop.discountsapi.app.dto.cupom.CupomDto;
+import com.github.andregpereira.resilientshop.discountsapi.app.dto.cupom.CupomRegistroDto;
+import com.github.andregpereira.resilientshop.discountsapi.app.service.CupomManutencaoService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@RequiredArgsConstructor
+@Slf4j
+@Validated
+@RestController
+@RequestMapping("/cupons")
+public class CupomController {
+
+    private final CupomManutencaoService manutencaoService;
+
+    @PostMapping
+    public ResponseEntity<CupomDto> registrar(@RequestBody CupomRegistroDto dto) {
+        CupomDto cupom = manutencaoService.registrar(dto);
+        URI uri = UriComponentsBuilder.fromPath("/cupons/{id}").buildAndExpand(cupom.id()).toUri();
+        return ResponseEntity.created(uri).body(cupom);
+    }
+
+}
