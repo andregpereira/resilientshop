6857148eac04f3370eb063f606033480835cee07
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-07-10 18:20:25 -0300

    Criar gateway de cupom

---
+++ resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/domain/gateway/CupomGateway.java
@@ -0,0 +1,9 @@
+package com.github.andregpereira.resilientshop.discountsapi.domain.gateway;
+
+import com.github.andregpereira.resilientshop.discountsapi.domain.model.Cupom;
+
+public interface CupomGateway {
+
+    Cupom registrar(Cupom cupom);
+
+}
