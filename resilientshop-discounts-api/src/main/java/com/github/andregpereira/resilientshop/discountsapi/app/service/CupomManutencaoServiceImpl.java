2f8ea439ac6f6fc8951268e788a9c6cf4c7a72b1
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-07-12 12:18:38 -0300

    Add @Slf4j

--- resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/app/service/CupomManutencaoServiceImpl.java
+++ resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/app/service/CupomManutencaoServiceImpl.java
@@ -2,13 +2,15 @@ package com.github.andregpereira.resilientshop.discountsapi.app.service;
 
 import com.github.andregpereira.resilientshop.discountsapi.app.dto.cupom.CupomDto;
 import com.github.andregpereira.resilientshop.discountsapi.app.dto.cupom.CupomRegistroDto;
-import com.github.andregpereira.resilientshop.discountsapi.app.mapper.cupom.CupomServiceMapper;
+import com.github.andregpereira.resilientshop.discountsapi.app.mapper.CupomServiceMapper;
 import com.github.andregpereira.resilientshop.discountsapi.domain.usecase.CreateCupom;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
+@Slf4j
 @Service
 @Transactional
 public non-sealed class CupomManutencaoServiceImpl implements CupomManutencaoService {
