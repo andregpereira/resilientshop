b5f51e6ca39aab917f75abae2833c06f76d78229
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-07-12 17:42:51 -0300

    Criar model Desconto

---
+++ resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/domain/model/Desconto.java
@@ -0,0 +1,52 @@
+package com.github.andregpereira.resilientshop.discountsapi.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Objects;
+import java.util.StringJoiner;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Desconto {
+
+    Long id;
+    BigDecimal percentual;
+    String tipoDesconto;
+    LocalDate dataCriacao;
+    LocalDate dataExpiracao;
+    boolean ativo;
+    Long idObjetoDoDesconto;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof Desconto desconto))
+            return false;
+        return ativo == desconto.ativo && Objects.equals(id, desconto.id) && Objects.equals(percentual,
+                desconto.percentual) && Objects.equals(tipoDesconto, desconto.tipoDesconto) && Objects.equals(
+                dataCriacao, desconto.dataCriacao) && Objects.equals(dataExpiracao,
+                desconto.dataExpiracao) && Objects.equals(idObjetoDoDesconto, desconto.idObjetoDoDesconto);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, percentual, tipoDesconto, dataCriacao, dataExpiracao, ativo, idObjetoDoDesconto);
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Desconto.class.getSimpleName() + "[", "]").add("id=" + id).add(
+                "percentual=" + percentual).add("tipoDesconto='" + tipoDesconto + "'").add(
+                "dataCriacao=" + dataCriacao).add("dataExpiracao=" + dataExpiracao).add("ativo=" + ativo).add(
+                "idObjetoDoDesconto=" + idObjetoDoDesconto).toString();
+    }
+
+}
