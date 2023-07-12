43e88cc957b0c6dda20ee7a14788945e8c7de8c2
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-07-12 11:34:45 -0300

    Mudar para Mapstruct

--- resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/app/mapper/CupomServiceMapper.java
+++ resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/app/mapper/CupomServiceMapper.java
@@ -1,10 +1,14 @@
-package com.github.andregpereira.resilientshop.discountsapi.app.mapper.cupom;
+package com.github.andregpereira.resilientshop.discountsapi.app.mapper;
 
 import com.github.andregpereira.resilientshop.discountsapi.app.dto.cupom.CupomDto;
 import com.github.andregpereira.resilientshop.discountsapi.app.dto.cupom.CupomRegistroDto;
 import com.github.andregpereira.resilientshop.discountsapi.domain.model.Cupom;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants.ComponentModel;
+import org.mapstruct.ReportingPolicy;
 
-public sealed interface CupomServiceMapper permits CupomServiceMapperImpl {
+@Mapper(componentModel = ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface CupomServiceMapper {
 
     CupomDto toCupomDto(Cupom cupom);
 
