24016ba263cdbe8f4f25cb6bbb52e00cdf4df33f
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-07-14 10:22:00 -0300

    Add operação de listagem de cupons

--- resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/domain/gateway/CupomGateway.java
+++ resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/domain/gateway/CupomGateway.java
@@ -1,9 +1,13 @@
 package com.github.andregpereira.resilientshop.discountsapi.domain.gateway;
 
 import com.github.andregpereira.resilientshop.discountsapi.domain.model.Cupom;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface CupomGateway {
 
     Cupom registrar(Cupom cupom);
+
+    Page<Cupom> findAll(Pageable pageable);
 
 }
