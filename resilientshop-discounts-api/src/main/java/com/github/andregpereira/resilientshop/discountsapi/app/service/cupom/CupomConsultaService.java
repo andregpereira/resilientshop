ee19c53b32b76241b0511c23437453179c54b9a0
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-07-14 20:16:57 -0300

    Refatorar service de cupom e reestruturar cpacote

--- resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/app/service/cupom/CupomConsultaService.java
+++ resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/app/service/cupom/CupomConsultaService.java
@@ -1,4 +1,4 @@
-package com.github.andregpereira.resilientshop.discountsapi.app.service;
+package com.github.andregpereira.resilientshop.discountsapi.app.service.cupom;
 
 import com.github.andregpereira.resilientshop.discountsapi.app.dto.cupom.CupomDto;
 import org.springframework.data.domain.Page;
--- resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/app/service/cupom/CupomManutencaoService.java
+++ resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/app/service/cupom/CupomManutencaoService.java
@@ -1,16 +1,17 @@
-package com.github.andregpereira.resilientshop.discountsapi.app.service;
+package com.github.andregpereira.resilientshop.discountsapi.app.service.cupom;
 
 import com.github.andregpereira.resilientshop.discountsapi.app.dto.cupom.CupomDto;
 import com.github.andregpereira.resilientshop.discountsapi.app.dto.cupom.CupomRegistroDto;
+import com.github.andregpereira.resilientshop.discountsapi.app.dto.cupom.CupomUpdateDto;
 
 public sealed interface CupomManutencaoService permits CupomManutencaoServiceImpl {
 
-    CupomDto registrar(CupomRegistroDto dto);
+    CupomDto criar(CupomRegistroDto dto);
 
-    String deactivate(Long id);
+    CupomDto update(Long id, CupomUpdateDto dto);
 
     String activate(Long id);
 
-    CupomDto update(Long id, CupomRegistroDto dto);
+    String deactivate(Long id);
 
 }
