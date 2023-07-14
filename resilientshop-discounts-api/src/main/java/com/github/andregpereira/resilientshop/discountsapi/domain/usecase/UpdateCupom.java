9472e4a27f71a18d8c7003123a89b2046349e6fc
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-07-14 19:25:43 -0300

    Criar use case de atualização de cupom

--- resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/domain/usecase/UpdateCupom.java
+++ resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/domain/usecase/UpdateCupom.java
@@ -1,5 +1,9 @@
 package com.github.andregpereira.resilientshop.discountsapi.domain.usecase;
 
+import com.github.andregpereira.resilientshop.discountsapi.domain.model.Cupom;
+
 public interface UpdateCupom {
+
+    Cupom update(Long id, Cupom cupom);
 
 }
