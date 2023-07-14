f9fa8b3f6a0ec1684bde3b2b6aa248ac6e42cc24
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-07-14 20:23:01 -0300

    Refatorar DTOs

--- resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/app/dto/cupom/CupomRegistroDto.java
+++ resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/app/dto/cupom/CupomRegistroDto.java
@@ -1,13 +1,16 @@
 package com.github.andregpereira.resilientshop.discountsapi.app.dto.cupom;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-public record CupomRegistroDto(String codigo,
-        Float percentual,
+public record CupomRegistroDto(@NotBlank(message = "O código é obrigatório") String codigo,
+        @NotNull(message = "O percentual é obrigatório") BigDecimal percentual,
         Integer qtdMinimaProdutos,
         BigDecimal valorMinimoPedido,
         BigDecimal descontoMaximo,
-        LocalDate dataExpiracao) {
+        @NotNull(message = "A data de expiração é obrigatória") LocalDate dataExpiracao) {
 
 }
