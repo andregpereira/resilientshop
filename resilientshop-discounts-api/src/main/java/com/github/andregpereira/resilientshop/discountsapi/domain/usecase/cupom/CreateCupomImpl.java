0a843abf24abc8d8c869a0d19909ec39b597c762
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-07-14 20:09:18 -0300

    Reestruturar pacote

--- resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/domain/usecase/cupom/CreateCupomImpl.java
+++ resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/domain/usecase/cupom/CreateCupomImpl.java
@@ -1,4 +1,4 @@
-package com.github.andregpereira.resilientshop.discountsapi.domain.usecase;
+package com.github.andregpereira.resilientshop.discountsapi.domain.usecase.cupom;
 
 import com.github.andregpereira.resilientshop.discountsapi.domain.gateway.CupomGateway;
 import com.github.andregpereira.resilientshop.discountsapi.domain.model.Cupom;
@@ -7,13 +7,14 @@ import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
 @Component
-public class FindByIdCupomImpl implements FindByIdCupom {
+public class CreateCupomImpl implements CreateCupom {
 
     private final CupomGateway gateway;
 
     @Override
-    public Cupom findById(Long id) {
-        return gateway.findById(id);
+    public Cupom criar(Cupom cupom) {
+        cupom.setAtivo(true);
+        return gateway.save(cupom);
     }
 
 }
