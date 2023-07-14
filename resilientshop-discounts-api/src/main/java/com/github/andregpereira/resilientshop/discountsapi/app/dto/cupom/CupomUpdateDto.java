57f75cfd630ec60404c74fae71a8b6fa4f044c52
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-07-14 20:22:38 -0300

    Criar DTO de atualização de cupom

---
+++ resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/app/dto/cupom/CupomUpdateDto.java
@@ -0,0 +1,17 @@
+package com.github.andregpereira.resilientshop.discountsapi.app.dto.cupom;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public record CupomUpdateDto(@NotBlank(message = "O código é obrigatório") String codigo,
+        @NotNull(message = "O percentual é obrigatório") BigDecimal percentual,
+        Integer qtdMinimaProdutos,
+        BigDecimal valorMinimoPedido,
+        BigDecimal descontoMaximo,
+        @NotNull(message = "A data de expiração é obrigatória") LocalDate dataExpiracao,
+        boolean ativo) {
+
+}
