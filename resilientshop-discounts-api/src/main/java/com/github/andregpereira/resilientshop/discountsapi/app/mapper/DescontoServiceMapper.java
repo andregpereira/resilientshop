f41aa1bd00e40d2248eafb86428d121f46c3e9ad
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-07-14 21:22:51 -0300

    Refatorar mapper

--- resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/app/mapper/DescontoServiceMapper.java
+++ resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/app/mapper/DescontoServiceMapper.java
@@ -10,8 +10,8 @@ import org.mapstruct.ReportingPolicy;
 @Mapper(componentModel = ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface DescontoServiceMapper {
 
-    DescontoDto toCupomDto(Desconto desconto);
+    DescontoDto toDescontoDto(Desconto desconto);
 
-    Desconto toCupom(DescontoRegistroDto dto);
+    Desconto toDesconto(DescontoRegistroDto dto);
 
 }
