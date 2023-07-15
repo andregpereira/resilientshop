055116b8ee074ff8f62b32056f2298b2d2bfc3de
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-07-14 21:00:31 -0300

    Add consultas de desconto ativo e inativo por id

--- resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/infra/repository/DescontoRepository.java
+++ resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/infra/repository/DescontoRepository.java
@@ -4,7 +4,13 @@ import com.github.andregpereira.resilientshop.discountsapi.infra.entity.Desconto
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface DescontoRepository extends JpaRepository<DescontoEntity, Long> {
+
+    Optional<DescontoEntity> findByIdAndAtivoTrue(Long id);
+
+    Optional<DescontoEntity> findByIdAndAtivoFalse(Long id);
 
 }
