982b6b982f26f410968498d8ed09918d16e36eda
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-08-03 17:23:59 -0300

    Estender classe abstrata CupomException

--- resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/cross/exception/CupomNotFoundException.java
+++ resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/cross/exception/CupomNotFoundException.java
@@ -1,23 +1,26 @@
 package com.github.andregpereira.resilientshop.discountsapi.cross.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatusCode;
 
 import java.text.MessageFormat;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
-public class CupomNotFoundException extends RuntimeException {
+public class CupomNotFoundException extends CupomException {
 
-    public CupomNotFoundException() {
-        super("Ops! Não há cupons cadastrados");
-    }
+    private static final HttpStatusCode status = HttpStatus.NOT_FOUND;
 
     public CupomNotFoundException(Long id) {
-        super(MessageFormat.format("Opa! Não foi encontrado um cupom com id {0}", id));
+        super(status, MessageFormat.format("Opa! Não foi encontrado um cupom com id {0}", id));
     }
 
     public CupomNotFoundException(Long id, boolean ativo) {
-        super(MessageFormat.format("Opa! Não foi encontrado um cupom {1} com id {0}", id, ativo ? "ativo" : "inativo"));
+        super(status, MessageFormat.format("Opa! Não foi encontrado um cupom {1} com id {0}", id,
+                ativo ? "ativo" : "inativo"));
+    }
+
+    @Override
+    public HttpStatusCode getStatusCode() {
+        return status;
     }
 
 }
