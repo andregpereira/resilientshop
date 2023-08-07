0b5bbd44911b583f5ce18134ebab9b1eb69abea1
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-08-07 10:15:43 -0300

    Mover métodos em comum para BaseGateway

--- resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/domain/gateway/DescontoGateway.java
+++ resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/domain/gateway/DescontoGateway.java
@@ -2,6 +2,6 @@ package com.github.andregpereira.resilientshop.discountsapi.domain.gateway;
 
 import com.github.andregpereira.resilientshop.discountsapi.domain.model.Desconto;
 
-public interface DescontoGateway extends Gateway<Desconto> {
+public interface DescontoGateway extends BaseGateway<Desconto> {
 
 }
