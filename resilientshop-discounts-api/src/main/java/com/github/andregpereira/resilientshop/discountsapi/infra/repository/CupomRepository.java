1fc763df9ea98a6a91f387c2d801e5a84be72424
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-08-07 15:19:41 -0300

    Criar consulta de cupom por código

--- resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/infra/repository/CupomRepository.java
+++ resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/infra/repository/CupomRepository.java
@@ -3,9 +3,13 @@ package com.github.andregpereira.resilientshop.discountsapi.infra.repository;
 import com.github.andregpereira.resilientshop.discountsapi.infra.entity.CupomEntity;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CupomRepository extends BaseRepository<CupomEntity> {
 
     boolean existsByCodigo(String codigo);
+
+    Optional<CupomEntity> findByCodigo(String codigo);
 
 }
