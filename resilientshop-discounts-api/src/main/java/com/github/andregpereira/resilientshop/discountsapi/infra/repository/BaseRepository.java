493021fcba1d307b3306cf296e45fc1f563b494b
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-08-07 15:19:14 -0300

    Criar consultas de entidade ativa e inativa

--- resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/infra/repository/BaseRepository.java
+++ resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/infra/repository/BaseRepository.java
@@ -1,13 +1,18 @@
 package com.github.andregpereira.resilientshop.discountsapi.infra.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @NoRepositoryBean
 public interface BaseRepository<T> extends JpaRepository<T, Long> {
+
+    Page<T> findAllByAtivoTrue(Pageable pageable);
+
+    Page<T> findAllByAtivoFalse(Pageable pageable);
 
     Optional<T> findByIdAndAtivoTrue(Long id);
 
