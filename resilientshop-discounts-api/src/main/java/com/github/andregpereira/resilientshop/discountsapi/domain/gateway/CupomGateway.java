8a23affa1cdf53162a8c1e72bdeb7879d0b2892b
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-08-07 10:15:43 -0300

    Mover métodos em comum para BaseGateway

--- resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/domain/gateway/CupomGateway.java
+++ resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/domain/gateway/CupomGateway.java
@@ -2,7 +2,7 @@ package com.github.andregpereira.resilientshop.discountsapi.domain.gateway;
 
 import com.github.andregpereira.resilientshop.discountsapi.domain.model.Cupom;
 
-public interface CupomGateway extends Gateway<Cupom> {
+public interface CupomGateway extends BaseGateway<Cupom> {
 
     boolean existsByCodigo(String codigo);
 
