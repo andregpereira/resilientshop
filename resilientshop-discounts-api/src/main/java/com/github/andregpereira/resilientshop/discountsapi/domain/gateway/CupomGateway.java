a4f031bf0dee99ea73590402ea95e43cf4b5558c
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-08-07 10:15:11 -0300

    Criar gateway base para métodos em comum

--- resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/domain/gateway/CupomGateway.java
+++ resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/domain/gateway/CupomGateway.java
@@ -1,19 +1,9 @@
 package com.github.andregpereira.resilientshop.discountsapi.domain.gateway;
 
 import com.github.andregpereira.resilientshop.discountsapi.domain.model.Cupom;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
-public interface CupomGateway {
+public interface CupomGateway extends Gateway<Cupom> {
 
-    Cupom save(Cupom cupom);
-
-    Page<Cupom> findAll(Pageable pageable);
-
-    Cupom findById(Long id);
-
-    Cupom findActivatedById(Long id);
-
-    Cupom findDeactivatedById(Long id);
+    boolean existsByCodigo(String codigo);
 
 }
