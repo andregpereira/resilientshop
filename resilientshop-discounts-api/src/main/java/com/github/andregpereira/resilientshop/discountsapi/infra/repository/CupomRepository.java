f75903fcd90f86a33db62a4907edb3248b40b903
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-08-07 10:02:53 -0300

    Mover consultas para BaseRepository

--- resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/infra/repository/CupomRepository.java
+++ resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/infra/repository/CupomRepository.java
@@ -1,16 +1,11 @@
 package com.github.andregpereira.resilientshop.discountsapi.infra.repository;
 
 import com.github.andregpereira.resilientshop.discountsapi.infra.entity.CupomEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface CupomRepository extends JpaRepository<CupomEntity, Long> {
+public interface CupomRepository extends BaseRepository<CupomEntity> {
 
-    Optional<CupomEntity> findByIdAndAtivoTrue(Long id);
-
-    Optional<CupomEntity> findByIdAndAtivoFalse(Long id);
+    boolean existsByCodigo(String codigo);
 
 }
--- resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/infra/repository/DescontoRepository.java
+++ resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/infra/repository/DescontoRepository.java
@@ -1,16 +1,9 @@
 package com.github.andregpereira.resilientshop.discountsapi.infra.repository;
 
 import com.github.andregpereira.resilientshop.discountsapi.infra.entity.DescontoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface DescontoRepository extends JpaRepository<DescontoEntity, Long> {
-
-    Optional<DescontoEntity> findByIdAndAtivoTrue(Long id);
-
-    Optional<DescontoEntity> findByIdAndAtivoFalse(Long id);
+public interface DescontoRepository extends BaseRepository<DescontoEntity> {
 
 }
