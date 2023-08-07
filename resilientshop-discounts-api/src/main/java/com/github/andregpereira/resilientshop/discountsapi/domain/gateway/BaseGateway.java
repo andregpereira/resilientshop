c1d9eff3d3a1de5db77a767a15b74d15b77409c7
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-08-07 10:15:11 -0300

    Criar gateway base para métodos em comum

---
+++ resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/domain/gateway/BaseGateway.java
@@ -0,0 +1,18 @@
+package com.github.andregpereira.resilientshop.discountsapi.domain.gateway;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface BaseGateway<T> {
+
+    T save(T t);
+
+    Page<T> findAll(Pageable pageable);
+
+    T findById(Long id);
+
+    T findAtivoById(Long id);
+
+    T findInativoById(Long id);
+
+}
