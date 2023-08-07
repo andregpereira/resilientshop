0ab8db68ebbf17f3e2e735736b2f01629d12841a
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-08-07 15:13:03 -0300

    Criar consultas de cupom ativo e inativo

---
+++ resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/domain/usecase/cupom/CupomFindAllInactiveUcImpl.java
@@ -0,0 +1,21 @@
+package com.github.andregpereira.resilientshop.discountsapi.domain.usecase.cupom;
+
+import com.github.andregpereira.resilientshop.discountsapi.domain.gateway.CupomGateway;
+import com.github.andregpereira.resilientshop.discountsapi.domain.model.Cupom;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class CupomFindAllInativoUcImpl implements CupomFindAllInativoUc {
+
+    private final CupomGateway gateway;
+
+    @Override
+    public Page<Cupom> findAllInativo(Pageable pageable) {
+        return gateway.findAllInativo(pageable);
+    }
+
+}
