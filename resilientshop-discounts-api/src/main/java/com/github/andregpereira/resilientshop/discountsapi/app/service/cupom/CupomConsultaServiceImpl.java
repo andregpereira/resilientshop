694d313721574adca642b8d980257ed2c357096d
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-08-07 15:14:45 -0300

    Criar consultas de cupom ativo, inativo e por código

--- resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/app/service/cupom/CupomConsultaServiceImpl.java
+++ resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/app/service/cupom/CupomConsultaServiceImpl.java
@@ -2,8 +2,7 @@ package com.github.andregpereira.resilientshop.discountsapi.app.service.cupom;
 
 import com.github.andregpereira.resilientshop.discountsapi.app.dto.cupom.CupomDto;
 import com.github.andregpereira.resilientshop.discountsapi.app.mapper.CupomServiceMapper;
-import com.github.andregpereira.resilientshop.discountsapi.domain.usecase.cupom.CupomFindAllUc;
-import com.github.andregpereira.resilientshop.discountsapi.domain.usecase.cupom.CupomFindByIdUc;
+import com.github.andregpereira.resilientshop.discountsapi.domain.usecase.cupom.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -16,7 +15,10 @@ import org.springframework.stereotype.Service;
 public non-sealed class CupomConsultaServiceImpl implements CupomConsultaService {
 
     private final CupomFindAllUc findAllUc;
+    private final CupomFindAllAtivoUc findAllAtivoUc;
+    private final CupomFindAllInativoUc findAllInativoUc;
     private final CupomFindByIdUc findByIdUc;
+    private final CupomFindByCodigoUc findByCodigoUc;
     private final CupomServiceMapper mapper;
 
     @Override
@@ -25,8 +27,23 @@ public non-sealed class CupomConsultaServiceImpl implements CupomConsultaService
     }
 
     @Override
+    public Page<CupomDto> consultarAtivo(Pageable pageable) {
+        return findAllAtivoUc.findAllAtivo(pageable).map(mapper::toCupomDto);
+    }
+
+    @Override
+    public Page<CupomDto> consultarInativo(Pageable pageable) {
+        return findAllInativoUc.findAllInativo(pageable).map(mapper::toCupomDto);
+    }
+
+    @Override
     public CupomDto consultarPorId(Long id) {
         return mapper.toCupomDto(findByIdUc.findById(id));
+    }
+
+    @Override
+    public CupomDto consultarPorCodigo(String codigo) {
+        return mapper.toCupomDto(findByCodigoUc.findByCodigo(codigo));
     }
 
 }
