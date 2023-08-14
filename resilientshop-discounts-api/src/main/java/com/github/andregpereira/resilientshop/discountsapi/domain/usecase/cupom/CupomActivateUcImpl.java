33d0df5f21afb3942ede789eb4d6734909a095a1
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-08-14 16:57:19 -0300

    Retornar void e remover sealed

--- resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/domain/usecase/cupom/CupomActivateUcImpl.java
+++ resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/domain/usecase/cupom/CupomActivateUcImpl.java
@@ -5,8 +5,6 @@ import com.github.andregpereira.resilientshop.discountsapi.domain.model.Cupom;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import java.text.MessageFormat;
-
 @RequiredArgsConstructor
 @Component
 public class CupomActivateUcImpl implements CupomActivateUc {
@@ -14,11 +12,10 @@ public class CupomActivateUcImpl implements CupomActivateUc {
     private final CupomGateway gateway;
 
     @Override
-    public String activate(Long id) {
+    public void activate(Long id) {
         Cupom cupom = gateway.findInativoById(id);
         cupom.setAtivo(true);
         gateway.save(cupom);
-        return MessageFormat.format("Cupom com id {0} ativado com sucesso!", id);
     }
 
 }
