b95b4e575bfb26020e9a4076eba75f46dea3bba6
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-08-14 10:37:42 -0300

    Validar data de expiração

--- resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/app/dto/desconto/DescontoCreateDto.java
+++ resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/app/dto/desconto/DescontoCreateDto.java
@@ -1,6 +1,7 @@
 package com.github.andregpereira.resilientshop.discountsapi.app.dto.desconto;
 
 import com.github.andregpereira.resilientshop.discountsapi.app.constant.TipoDesconto;
+import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
@@ -8,7 +9,8 @@ import java.time.LocalDate;
 
 public record DescontoCreateDto(@NotNull(message = "O percentual é obrigatório") BigDecimal percentual,
         @NotNull(message = "O tipo de desconto é obrigatório") TipoDesconto tipoDesconto,
-        @NotNull(message = "A data de expiração é obrigatória") LocalDate dataExpiracao,
-        @NotNull(message = "O objeto do desconto é obrigatório") Long idObjetoDoDesconto) {
+        @NotNull(message = "A data de expiração é obrigatória") @Future(
+                message = "A data de expiração deve ser uma data futura") LocalDate dataExpiracao,
+        @NotNull(message = "O id do objeto do desconto é obrigatório") Long idObjetoDoDesconto) {
 
 }
