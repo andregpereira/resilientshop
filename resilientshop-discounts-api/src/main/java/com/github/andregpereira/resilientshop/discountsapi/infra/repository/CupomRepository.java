93e3ee7f0d243ff265848a1fde6653707dcaeb9a
André Garcia <61523970+andregpereira@users.noreply.github.com>
2023-09-05 11:40:48 -0300

    Refatorar código

--- resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/infra/repository/CupomRepository.java
+++ resilientshop-discounts-api/src/main/java/com/github/andregpereira/resilientshop/discountsapi/infra/repository/CupomRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface CupomRepository extends BaseRepository<CupomEntity> {
+public non-sealed interface CupomRepository extends BaseRepository<CupomEntity> {
 
     boolean existsByCodigo(String codigo);
 
