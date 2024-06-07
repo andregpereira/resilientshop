17c44b36381c952a7afe22ea17ef7d5999516bf4
André Garcia <61523970+andregpereira@users.noreply.github.com>
2024-06-07 16:54:20 -0300

    refatorar metodos depreciados

--- resilientshop-authentication-api/src/main/java/com/github/andregpereira/resilientshop/authenticationapi/infra/security/jwt/JwtProviderImpl.java
+++ resilientshop-authentication-api/src/main/java/com/github/andregpereira/resilientshop/authenticationapi/infra/security/jwt/JwtProviderImpl.java
@@ -1,19 +1,18 @@
 package com.github.andregpereira.resilientshop.authenticationapi.infra.security.jwt;
 
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
-import java.security.Key;
+import javax.crypto.SecretKey;
 import java.time.Clock;
 import java.time.Instant;
 import java.util.Date;
 import java.util.List;
 
-@Service
+@Component
 public class JwtProviderImpl implements JwtProvider {
 
     private static final String SECRET_KEY = "Som35ecretK3y109jP2n8PaMS05mDKAPOjd23ur98yoej";
@@ -21,16 +20,21 @@ public class JwtProviderImpl implements JwtProvider {
     @Override
     public String gerarToken(String email, List<? extends GrantedAuthority> roles) {
         Date inicio = Date.from(Instant.now(Clock.systemUTC()));
-        return Jwts.builder().setSubject(email).claim("role", roles.get(0)).setIssuedAt(inicio).setExpiration(
-                new Date(inicio.getTime() + 3600000)).signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
+        return Jwts.builder()
+                .subject(email)
+                .claim("role", roles.get(0))
+                .issuedAt(inicio)
+                .expiration(new Date(inicio.getTime() + 3600000))
+                .signWith(getSignKey())
+                .compact();
     }
 
     @Override
     public void validarToken(String token) {
-        Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token);
+        Jwts.parser().verifyWith(getSignKey()).build().parseSignedClaims(token);
     }
 
-    private Key getSignKey() {
+    private SecretKey getSignKey() {
         return Keys.hmacShaKeyFor(Decoders.BASE64.decode(SECRET_KEY));
     }
 
